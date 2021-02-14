3f8af59c6e4866f157af007997648cd4b7c4d31b
margegaj24 <mario.margegaj@gmail.com>
2021-02-14 18:45:13 +0100

    Update

--- src/components/forms/addStudent.js
+++ src/components/forms/addStudent.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { Modal, Form, Input } from "antd";
+import { Modal, Form, Input, Select } from "antd";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const AddStudentForm = ({ visible, onCreate, onCancel }) => {
   const [form] = Form.useForm();
+  const { Option } = Select;
+  const [availableCourses, setAvailableCourses] = useState([]);
+  const [courseOptions, setCourseOptions] = useState([]);
+
+  useEffect(() => loadAvailableCourses(), []);
+
+  const loadAvailableCourses = async () => {
+    var response = await axios.get("http://localhost:5000/courses");
+    var courses = [];
+    var allCourses = [];
+    response.data.forEach((course, index) => {
+      courses.push(<Option key={index}>{course.name}</Option>);
+      allCourses.push(course);
+    });
+    setAvailableCourses(allCourses);
+    setCourseOptions(courses);
+  };
+
   return (
     <Modal
       visible={visible}
@@ -12,22 +31,18 @@ const AddStudentForm = ({ visible, onCreate, onCancel }) => {
       cancelText="Cancel"
       onCancel={onCancel}
       onOk={() => {
-        form
-          .validateFields()
-          .then((values) => {
-            console.log(values);
-            axios
-              .post("http://localhost:5000/students", values)
-              .then((response) => {
-                if (response.data.error) alert(response.data.error);
-                else alert(response.data.message);
-              })
-              .catch((error) => alert(error.message));
-            onCreate();
+        var submitedValues = form.getFieldsValue();
+
+        submitedValues.courses = submitedValues.courses.map((indexEl) => availableCourses[parseInt(indexEl)]._id);
+        console.log(submitedValues);
+        axios
+          .post("http://localhost:5000/students", submitedValues)
+          .then((response) => {
+            if (response.data.error) alert(response.data.error);
+            else alert(response.data.message);
           })
-          .catch((info) => {
-            console.log("Validate Failed: ", info);
-          });
+          .catch((error) => alert(error.message));
+        onCreate();
       }}
     >
       <Form
@@ -61,6 +76,11 @@ const AddStudentForm = ({ visible, onCreate, onCancel }) => {
           ]}
         >
           <Input />
+        </Form.Item>
+        <Form.Item name="courses" label="Select Courses">
+          <Select mode="tags" style={{ width: "100%" }} tokenSeparators={["   "]}>
+            {courseOptions}
+          </Select>
         </Form.Item>
       </Form>
     </Modal>
